84c8764d01916b55eecaaf70817e25a62687e682
Matheus Heinrique <74678483+K4binho@users.noreply.github.com>
2022-03-20 13:48:09 -0400

    Update 2.includes.js

--- 2.includes.js
+++ 2.includes.js
@@ -1,4 +1,4 @@
 const frase =  "Eu estou aprendendo aprogramar na Cubos Academy";
 
-//buscou uma palavra chave dentro da const
+//buscou uma palavra chave dentro da const retonando uma Boolean true/false
 console.log(frase.includes("Cubos", 3)); //Tem que ser identica 

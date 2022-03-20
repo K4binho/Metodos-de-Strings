19afadc8508977b0d47f6661e9ee5d57ecfa65b4
Matheus Heinrique <74678483+K4binho@users.noreply.github.com>
2022-03-20 13:50:24 -0400

    Update 4.exercicio.slice.js

--- 4.exercicio.slice.js
+++ 4.exercicio.slice.js
@@ -1,6 +1,8 @@
 const cidade = "Manaus-Am"
 
-//console.log(cidade.slice(-2)); //resolvendo do jeito mais facil
+//console.log(cidade.slice(-2)); obs: outro método de resolver mas de um jeito mais facil
+
+
 const penultimoIndex = cidade.length - 2;
 let estado = cidade.slice(penultimoIndex);
 console.log(estado)
